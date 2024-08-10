11288709ae5e7a370132ea4ea2264269ae2529a5
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-10 18:32:43 +0530

    feat: add feature component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Feature from "./components/Feature";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -6,6 +7,7 @@ export default function App() {
     <>
       <Navbar />
       <Hero />
+      <Feature />
     </>
   );
 }
