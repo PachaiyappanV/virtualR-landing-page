366f6a1be4c3cae0df513f9bb6ee891322560024
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-10 16:03:56 +0530

    feat: add mobile navebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants/index";
 const Navbar = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
   return (
     <nav className="sticky top-0 backdrop-blur-lg z-50 py-3 border-b border-neutral-700">
       <div className="container mx-auto px-4 relative text-sm">
@@ -27,7 +33,34 @@ const Navbar = () => {
               Create an account
             </a>
           </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button type="button" onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
         </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 flex flex-col w-full z-20 p-12 bg-neutral-900 justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded-md capitalize">
+                sign in
+              </a>
+              <a
+                href="#"
+                className="py-2 px-3 border rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+              >
+                Create an account
+              </a>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
